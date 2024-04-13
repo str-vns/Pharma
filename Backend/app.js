13e0b57cf1eeb15871d124b12103af9439b56b3c
Flappy <stvnsbarrantes@gmail.com>
2024-04-13 19:42:31 +0800

    app

--- Backend/app.js
+++ Backend/app.js
@@ -4,10 +4,10 @@ const app = express()
 const cookiesParser = require('cookie-parser')
 const cors = require("cors")
 const corsOptions = require('./config/corsOptions')
-
+const users = require('./routes/user')
 app.use(cors(corsOptions))
 app.use(cookiesParser())
 app.use(express.json())
 
-// app.use("/api/v1", )
+app.use("/api/v1", users)
 module.exports = app

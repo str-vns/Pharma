9952f0403be135ee5ad01d589ce5b24bfa4c5cac
Flappy <stvnsbarrantes@gmail.com>
2024-04-13 19:44:17 +0800

    update Phat

--- Backend/constants/path.js
+++ Backend/constants/path.js
@@ -2,6 +2,6 @@ module.exports = {
     USERS: "/users",
     USER_ID: "/user/:id",
     USER_EDIT_ID: "/user/edit/:id",
-    RESTORE_ID: "/user/restore/:id",
+    RESTORE_ID: "/restore/:id",
     REGISTER: "/register"
 }

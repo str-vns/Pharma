e84d0d2d69f42108f9b88dcccc8606ef3ae5fb2b
Flappy <stvnsbarrantes@gmail.com>
2024-04-15 15:15:08 +0800

    constant update

--- Backend/constants/path.js
+++ Backend/constants/path.js
@@ -2,6 +2,10 @@ module.exports = {
     USERS: "/users",
     USER_ID: "/user/:id",
     USER_EDIT_ID: "/user/edit/:id",
-    RESTORE_ID: "/restore/:id",
-    REGISTER: "/register"
+    RESTORE_USER_ID: "/user/restore/:id",
+    REGISTER: "/register",
+    BRAND: "/brands",
+    BRAND_ID: "/brand/:id",
+    BRAND_EDIT_ID: "/brand/edit/:id",
+    RESTORE_BRAND_ID: "/brand/restore/:id",
 }
